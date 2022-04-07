1382d8f66b6a4a2ebc285dde556dc1d876c9bd52
Axel801 <axelloupias@gmail.com>
2022-04-07 20:25:29 +0200

    Update utils

--- utils.sh
+++ utils.sh
@@ -43,9 +43,29 @@ function verifyResult() {
     fi
 }
 
+function setVariables(){
+    USING_ORG=$1
+    if [ $USING_ORG -eq "centroarte" ]; then
+
+    export CORE_PEER_TLS_ENABLED=true
+    export PEER0_ORG1_CA=${PWD}/organizations/peerOrganizations/centroarte.example.com/peers/peer0.centroarte.example.com/tls/ca.crt
+    export CORE_PEER_LOCALMSPID="CentroArteMSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/centroarte.example.com/users/Admin@centroarte.example.com/msp
+    export CORE_PEER_ADDRESS=localhost:7051
+
+  elif [ $USING_ORG -eq "anabelsegura" ]; then
+    export PEER0_ORG2_CA=${PWD}/organizations/peerOrganizations/anabelsegura.example.com/peers/peer0.anabelsegura.example.com/tls/ca.crt
+    export CORE_PEER_LOCALMSPID="AnabelSeguraMSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG2_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/anabelsegura.example.com/users/Admin@anabelsegura.example.com/msp
+    export CORE_PEER_ADDRESS=localhost:9051
+}
+
 export -f errorln
 export -f successln
 export -f infoln
 export -f warnln
 
 export -f verifyResult
+export -f setVariables
