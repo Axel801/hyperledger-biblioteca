38cad6997fe28fbd09beaf860feb59e4ede1e892
Axel801 <axelloupias@gmail.com>
2022-04-15 11:26:36 +0200

    Update chaincode

--- chaincodes/libros/trazabilidad.go
+++ chaincodes/libros/trazabilidad.go
@@ -1,56 +1,59 @@
 package main
 
-import (
-  "encoding/json"
-  "fmt"
-  "log"
-  "time"
+  import (
+    "encoding/json"
+    "fmt"
+    "log"
+    "time"
 
-  "github.com/golang/protobuf/ptypes"
-  "github.com/satori/go.uuid"
-  "github.com/hyperledger/fabric-contract-api-go/contractapi"
-)
+    "github.com/golang/protobuf/ptypes"
+    "github.com/satori/go.uuid"
+    "github.com/hyperledger/fabric-contract-api-go/contractapi"
 
-type SmartContract struct {
+    "github.com/hyperledger/fabric-chaincode-go/pkg/cid"
+  )
+
+  type SmartContract struct {
       contractapi.Contract
-    }
+  }
 
-type Farmaco struct {
-      ID             string `json:"ID"`
-      Ph             string `json:"ph"`
-      Temperature    int    `json:"temperature"`
-      User           string `json:"user"`
-	    Geo            string `json:"geo"`
-      Estado         string `json:"estado"`
-      Extradata      string `json:"extradata"`
-	}
+  type Libro struct {
+    ID             string `json:"ID"`
+    Nombre         string `json:"nombre"`
+    Owner          string `json:"owner"`
+    Estado         string `json:"estado"`
+  }
 
-	type HistoryQueryResult struct {
-		Record    *Farmaco    `json:"record"`
-		TxId     string    `json:"txId"`
+  type HistoryQueryResult struct {
+		Record    *Libro    `json:"record"`
+		TxId      string    `json:"txId"`
 		Timestamp time.Time `json:"timestamp"`
 		IsDelete  bool      `json:"isDelete"`
 	}
 
-func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
+  func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
+    
+    mspid, _:=cid.GetMSPID(ctx.GetStub())
+    log.Println("Lo ha lanzado: ", mspid)
+    
+    myuuids := uuid.NewV4().String()
+    fmt.Println("Your UUID is:", myuuids)
 
-	myuuids := uuid.NewV4().String()
-	fmt.Println("Your UUID is:", myuuids)
-
-	myuuid2 := uuid.NewV4().String()
-	fmt.Println("Your UUID2 is:", myuuid2)
-	
-    farmacos := []Farmaco{
-      {ID: myuuids, Ph: "5.5", Temperature: 12, User: "USR122", Geo: "62 , -11", Estado: "Fabricado", Extradata: "-"},
-      {ID: myuuid2, Ph: "5.4", Temperature: 12, User: "USR099", Geo: "45 , -66", Estado: "Fabricando", Extradata: "-"},
+    myuuid2 := uuid.NewV4().String()
+    fmt.Println("Your UUID2 is:", myuuid2)
+    
+    libros := []Libro{
+      {ID: myuuids, Nombre: "El nombre del viento", Owner: mspid, Estado: "Disponible"},
+      {ID: myuuid2, Nombre: "El imperio final", Owner: mspid, Estado: "Disponible"},
     }
-    for _, farmaco := range farmacos {
-      farmacoJSON, err := json.Marshal(farmaco)
+
+    for _, libro := range libros {
+      libroJSON, err := json.Marshal(libro)
       if err != nil {
         return err
       }
     
-      err = ctx.GetStub().PutState(farmaco.ID, farmacoJSON)
+      err = ctx.GetStub().PutState(libro.ID, libroJSON)
       if err != nil {
         return fmt.Errorf("failed to put to world state. %v", err)
       }
@@ -59,210 +62,205 @@ func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) 
     return nil
   }
 
-func (s *SmartContract) CreateFarmaco(ctx contractapi.TransactionContextInterface, ph string, temperature int, user string, geo string, estado string, extradata string) error {
+  func (s *SmartContract) CreateLibro(ctx contractapi.TransactionContextInterface, nombre string, owner string, estado string) error {
 
-	myuuid := uuid.NewV4().String()
-	fmt.Println("Nuevo fármaco UUID:", myuuid)
+    myuuid := uuid.NewV4().String()
+    fmt.Println("Nuevo libro UUID:", myuuid)
 
-    farmaco := Farmaco{
+    libro := Libro{
       ID:          myuuid,
-      Ph:          ph,
-      Temperature: temperature,
-      User:        user,
-	    Geo:         geo,
+      Nombre:      nombre,
+      Owner:       owner,
       Estado:		   estado,
-      Extradata:   extradata,
     }
-    farmacoJSON, err := json.Marshal(farmaco)
+      
+    libroJSON, err := json.Marshal(libro)
     if err != nil {
       return err
     }
 
-    return ctx.GetStub().PutState(myuuid, farmacoJSON)
+    return ctx.GetStub().PutState(myuuid, libroJSON)
   }
 
-// ReadFarmaco devuelve el Farmaco almacenado en el World State con su última modificación
-   func (s *SmartContract) ReadFarmaco(ctx contractapi.TransactionContextInterface, id string) (*Farmaco, error) {
-    farmacoJSON, err := ctx.GetStub().GetState(id)
+  // ReadLibro devuelve el Libro almacenado en el World State con su última modificación
+  func (s *SmartContract) ReadLibro(ctx contractapi.TransactionContextInterface, id string) (*Libro, error) {
+    libroJSON, err := ctx.GetStub().GetState(id)
     if err != nil {
       return nil, fmt.Errorf("Error al leer de Fabric: %v", err)
     }
-    if farmacoJSON == nil {
-      return nil, fmt.Errorf("El fármaco %s no existe", id)
+    if libroJSON == nil {
+      return nil, fmt.Errorf("El libro %s no existe", id)
     }
 
-    var farmaco Farmaco
-    err = json.Unmarshal(farmacoJSON, &farmaco)
+    var libro Libro
+    err = json.Unmarshal(libroJSON, &libro)
     if err != nil {
       return nil, err
     }
 
-    return &farmaco, nil
+    return &libro, nil
   }
 
-func (s *SmartContract) UpdateFarmaco(ctx contractapi.TransactionContextInterface, id string, ph string, temperature int, user string, geo string, estado string, extradata string) error {
-  exists, err := s.FarmacoExists(ctx, id)
-  if err != nil {
-      return err
-    }
-  if !exists {
-      return fmt.Errorf("El fármaco %s no existe", id)
-    }
-
-  farmaco := Farmaco{
-      ID:          id,
-      Ph:          ph,
-      Temperature: temperature,
-      User:        user,
-	    Geo:         geo,
-      Estado:		   estado,
-      Extradata:   extradata,
-    }
-  
-  farmacoJSON, err := json.Marshal(farmaco)
-  
-  if err != nil {
-      return err
-    }
-
-    return ctx.GetStub().PutState(id, farmacoJSON)
-  }
-
-  // DeleteFarmaco elimina a un Farmaco del world state
-  func (s *SmartContract) DeleteFarmaco(ctx contractapi.TransactionContextInterface, id string) error {
-    exists, err := s.FarmacoExists(ctx, id)
+  func (s *SmartContract) UpdateLibro(ctx contractapi.TransactionContextInterface, id string, nombre string, owner string, estado string) error {
+    exists, err := s.LibroExists(ctx, id)
     if err != nil {
       return err
     }
     if !exists {
-      return fmt.Errorf("the farmaco %s does not exist", id)
+      return fmt.Errorf("El libro %s no existe", id)
+    }
+
+    libro := Libro{
+        ID:          id,
+        Nombre:      nombre,
+        Owner:       owner,
+        Estado:		   estado,
+    }
+  
+    libroJSON, err := json.Marshal(libro)
+  
+    if err != nil {
+      return err
+    }
+
+    return ctx.GetStub().PutState(id, libroJSON)
+  }
+
+  // DeleteLibro elimina a un Libro del world state
+  func (s *SmartContract) DeleteLibro(ctx contractapi.TransactionContextInterface, id string) error {
+    exists, err := s.LibroExists(ctx, id)
+    if err != nil {
+      return err
+    }
+    if !exists {
+      return fmt.Errorf("the libro %s does not exist", id)
     }
 
     return ctx.GetStub().DelState(id)
   }
 
-// FarmacoExists devuelve un true si existe el ID del Farmaco consultado
-   func (s *SmartContract) FarmacoExists(ctx contractapi.TransactionContextInterface, id string) (bool, error) {
-    farmacoJSON, err := ctx.GetStub().GetState(id)
+  // LibroExists devuelve un true si existe el ID del Libro consultado
+  func (s *SmartContract) LibroExists(ctx contractapi.TransactionContextInterface, id string) (bool, error) {
+    libroJSON, err := ctx.GetStub().GetState(id)
     if err != nil {
       return false, fmt.Errorf("failed to read from world state: %v", err)
     }
 
-    return farmacoJSON != nil, nil
+    return libroJSON != nil, nil
   }
 
-// TransferFarmaco actualiza el dueño del Farmaco en el world state
-   func (s *SmartContract) TransferFarmaco(ctx contractapi.TransactionContextInterface, id string, newUser string) error {
-    farmaco, err := s.ReadFarmaco(ctx, id)
+  // TransferLibro actualiza el dueño del Libro en el world state
+  func (s *SmartContract) TransferLibro(ctx contractapi.TransactionContextInterface, id string, newUser string) error {
+    libro, err := s.ReadLibro(ctx, id)
     if err != nil {
       return err
     }
 
-    farmaco.User = newUser
-    farmacoJSON, err := json.Marshal(farmaco)
+    libro.User = newUser
+    libroJSON, err := json.Marshal(libro)
     if err != nil {
       return err
     }
 
-    return ctx.GetStub().PutState(id, farmacoJSON)
+    return ctx.GetStub().PutState(id, libroJSON)
   }
 
-// StatusFarmaco actualiza el estado del Farmaco en el world state
-func (s *SmartContract) StatusFarmaco(ctx contractapi.TransactionContextInterface, id string, newStatus string) error {
-    farmaco, err := s.ReadFarmaco(ctx, id)
+  // StatusLibro actualiza el estado del Libro en el world state
+  func (s *SmartContract) StatusLibro(ctx contractapi.TransactionContextInterface, id string, newStatus string) error {
+    libro, err := s.ReadLibro(ctx, id)
     if err != nil {
       return err
     }
 
-    farmaco.Estado = newStatus
-    farmacoJSON, err := json.Marshal(farmaco)
+    libro.Estado = newStatus
+    libroJSON, err := json.Marshal(libro)
     if err != nil {
       return err
     }
 
-    return ctx.GetStub().PutState(id, farmacoJSON)
+    return ctx.GetStub().PutState(id, libroJSON)
   }
 
-// GetAllFarmacos devuelve todos los Farmacos del world state
-   func (s *SmartContract) GetAllFarmacos(ctx contractapi.TransactionContextInterface) ([]*Farmaco, error) {
+  // GetAllLibros devuelve todos los Libros del world state
+  func (s *SmartContract) GetAllLibros(ctx contractapi.TransactionContextInterface) ([]*Libro, error) {
     resultsIterator, err := ctx.GetStub().GetStateByRange("", "")
     if err != nil {
       return nil, err
     }
     defer resultsIterator.Close()
 
-    var farmacos []*Farmaco
+    var libros []*Libro
     for resultsIterator.HasNext() {
       queryResponse, err := resultsIterator.Next()
       if err != nil {
         return nil, err
       }
 
-      var farmaco Farmaco
-      err = json.Unmarshal(queryResponse.Value, &farmaco)
+      var libro Libro
+      err = json.Unmarshal(queryResponse.Value, &libro)
       if err != nil {
         return nil, err
       }
-      farmacos = append(farmacos, &farmaco)
+      libros = append(libros, &libro)
     }
 
-    return farmacos, nil
+    return libros, nil
   }
 
-// GetFarmacoHistory devuelve la información histórica custodiada por el ledger
-func (t *SmartContract) GetFarmacoHistory(ctx contractapi.TransactionContextInterface, farmacoID string) ([]HistoryQueryResult, error) {
-	log.Printf("GetFarmacoHistory: ID %v", farmacoID)
+  // GetLibroHistory devuelve la información histórica custodiada por el ledger
+  func (t *SmartContract) GetLibroHistory(ctx contractapi.TransactionContextInterface, libroID string) ([]HistoryQueryResult, error) {
+	  log.Printf("GetLibroHistory: ID %v", libroID)
 
-	resultsIterator, err := ctx.GetStub().GetHistoryForKey(farmacoID)
-	if err != nil {
-		return nil, err
-	}
-	defer resultsIterator.Close()
+	  resultsIterator, err := ctx.GetStub().GetHistoryForKey(libroID)
+	  if err != nil {
+  		return nil, err
+  	}
+  	defer resultsIterator.Close()
 
-	var records []HistoryQueryResult
-	for resultsIterator.HasNext() {
-		response, err := resultsIterator.Next()
-		if err != nil {
-			return nil, err
-		}
+    var records []HistoryQueryResult
+    for resultsIterator.HasNext() {
+      response, err := resultsIterator.Next()
+      if err != nil {
+        return nil, err
+      }
 
-		var farmaco Farmaco
-		if len(response.Value) > 0 {
-			err = json.Unmarshal(response.Value, &farmaco)
-			if err != nil {
-				return nil, err
-			}
-		} else {
-			farmaco = Farmaco{
-				ID: farmacoID,
-			}
-		}
+      var libro Libro
+      if len(response.Value) > 0 {
+        err = json.Unmarshal(response.Value, &libro)
+        if err != nil {
+          return nil, err
+        }
+      } else {
+        libro = Libro{
+          ID: libroID,
+        }
+      }
 
-		timestamp, err := ptypes.Timestamp(response.Timestamp)
-		if err != nil {
-			return nil, err
-		}
+      timestamp, err := ptypes.Timestamp(response.Timestamp)
+      if err != nil {
+        return nil, err
+      }
 
-		record := HistoryQueryResult{
-			TxId:      response.TxId,
-			Timestamp: timestamp,
-			Record:    &farmaco,
-			IsDelete:  response.IsDelete,
-		}
-		records = append(records, record)
-	}
-
-	return records, nil
-}
-
-  func main() {
-    farmacoChaincode, err := contractapi.NewChaincode(&SmartContract{})
-    if err != nil {
-      log.Panicf("Error creating farmaco-transfer-basic chaincode: %v", err)
+      record := HistoryQueryResult{
+        TxId:      response.TxId,
+        Timestamp: timestamp,
+        Record:    &farmaco,
+        IsDelete:  response.IsDelete,
+      }
+      records = append(records, record)
     }
 
-    if err := farmacoChaincode.Start(); err != nil {
-      log.Panicf("Error starting farmaco-transfer-basic chaincode: %v", err)
+    return records, nil
+  }
+
+  func main() {
+    libroChaincode, err := contractapi.NewChaincode(&SmartContract{})
+    if err != nil {
+      log.Panicf("Error creating libro-transfer-basic chaincode: %v", err)
+    }
+
+    if err := libroChaincode.Start(); err != nil {
+      log.Panicf("Error starting libro-transfer-basic chaincode: %v", err)
     }
   }
 
